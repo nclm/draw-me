661f83c62e7b2ba28c8de06d5bbb9f8b780ec36c
nclm <nclm@users.noreply.github.com>
2015-04-25 18:32:01 +0200

    gallery styles

--- drawme.js
+++ drawme.js
@@ -11,14 +11,17 @@ function drawme() {
 adjective1 = pick.apply(this, adjectives);
 adjective2 = pick.apply(this, adjectives);
 adjective3 = pick.apply(this, adjectives);
+
 // Nouns
 noun1 = pick.apply(this, nouns);
 noun2 = pick.apply(this, nouns);
 noun3 = pick.apply(this, nouns);
+
 // Transitive verbs
 verb1 = pick.apply(this, verbs);
 verb2 = pick.apply(this, verbs);
 verb3 = pick.apply(this, verbs);
+
 // Contexts
 context1 = pick.apply(this, contexts);
 context2 = pick.apply(this, contexts);
@@ -37,22 +40,43 @@ sentences = new Array(
 sentence = sentences[Math.floor(Math.random()*sentences.length)]+'.';
 
 // Write it!
-document.getElementById('what').innerHTML = sentence;
+var what =  document.getElementById('what')
+
+if (what) {
+ what.innerHTML = sentence;
+}
 
 };
 
-// Turn the background into a random colour
-function colour() {
+// Make colours 
+function colours() {
+ r = Math.floor(Math.random()*200);
+ g = Math.floor(Math.random()*200);
+ b = Math.floor(Math.random()*200);
+}
 
-var r = Math.floor(Math.random()*200);
-var g = Math.floor(Math.random()*200);
-var b = Math.floor(Math.random()*200);
-document.getElementById('page').style.background = 'rgb('+r+','+g+','+b+')';
+// Colour things
+function colouring() {
+
+var home = document.getElementById('home');
+var quotes = document.querySelectorAll('#gallery blockquote');
+
+if (home) {
+ colours();
+ home.style.background = 'rgb('+r+','+g+','+b+')';
+}
+
+if (quotes) {
+ for (i = 0; i < quotes.length; i++) {
+  colours();
+  quotes[i].style.color = 'rgb('+r+','+g+','+b+')';
+ }
+}
 
 };
 
 // Do everything
 function update() {
  drawme();
- colour();
+ colouring();
 };
